aa2f678692e6c2626ff1586f39e84c553a377d9c
KalDoe02 <KalDoe02@DV2104>
2022-04-19 16:14:55 +0200

    Button hinzugefügt

--- LaborGit/MainWindow.xaml.cs
+++ LaborGit/MainWindow.xaml.cs
@@ -24,5 +24,15 @@ namespace LaborGit
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
